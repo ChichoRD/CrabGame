56d2d5a99ed4d81e68d7ba50827d7dc7eed45d81
David13fb <146523610+David13fb@users.noreply.github.com>
2024-01-19 13:12:55 +0100

    movimiento basico

---
+++ Assets/Systems/MovementSystem/Performer/PlanarMovementPerformer.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+namespace MovementSystem.Performer
+{
+    internal class PlanarMovementPerformer : MonoBehaviour, IMovementPerformer<Vector2>
+    {
+        [SerializeField]
+        private float _movementspeed = 1.0f;
+        public bool TryPerformMovement(Rigidbody2D rigidbody, Vector2 direction)
+        {
+            Vector2 actualspeed = rigidbody.velocity;
+            Vector2 targetspeed = _movementspeed * direction;
+            Vector2 finalspeed = (targetspeed - actualspeed) * 0.5f;
+
+            rigidbody.AddForce(finalspeed * rigidbody.mass / Time.fixedDeltaTime);
+            return true;
+
+        }
+
+
+
+    }
+}
+
+
