7f7306dd8295a9454c69ed6a5b9588db3dcce4b7
ChichoRD <chichord2005@hotmail.com>
2024-01-21 02:42:52 +0100

    Hitbox abilities

---
+++ Assets/Systems/Ability System/Ability/Flyweight/HitboxDamageAbilityFlyweightSettings.cs
@@ -0,0 +1,24 @@
+﻿using AbilitySystem.Input.Service;
+using AbilitySystem.Status.Components;
+using UnityEngine;
+
+namespace AbilitySystem.Ability.Flyweight
+{
+    [CreateAssetMenu(fileName = "HitboxDamageAbilityFlyweightSettings", menuName = "Ability System/Ability Flyweight/Hitbox Damage Ability")]
+    internal class HitboxDamageAbilityFlyweightSettings : AbilityFlyweightSettings
+    {
+        [SerializeField]
+        [Min(0.0f)]
+        private float _damage;
+
+        [SerializeField]
+        private LayerMask _layerMask;
+
+        public override IAbility Create(IAbilityInputService abilityDataService)
+        {
+            return abilityDataService.TryGetInput(out Bounds bounds)
+                ? new HitboxAbility<IDamageable>(bounds, _layerMask, new DamageAbility(_damage))
+                : NullAbility.Instance;
+        }
+    }
+}
---
+++ Assets/Systems/Ability System/Input/Service/BoundsInputService.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace AbilitySystem.Input.Service
+{
+    internal class BoundsInputService : MonoBehaviour, IAbilityInputService<Bounds>
+    {
+        [SerializeField]
+        private Transform _boundsOriginTransform;
+
+        [SerializeField]
+        private Vector3 _size;
+
+        public Bounds GetInput()
+        {
+            return new Bounds(_boundsOriginTransform.position, _size);
+        }
+    }
+}
